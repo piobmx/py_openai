7f5ffc3cadbb2189c05afab8aa8d0e5e47d853ae
Zhangyi Wu <ng@MacBook-Pro.local>
2023-10-05 22:06:46 +0200

    vercel api

--- api/server.py
+++ api/server.py
@@ -1,7 +1,79 @@
 from flask import Flask, request, jsonify, Response
 from flask_cors import CORS
-from oai import OaiAPI
+import openai
 
+prompt = """
+uniform vec2 u_resolution;
+uniform float u_time;
+varying vec2 vUv;
+
+void main(){
+ // [The Missing Part]
+}
+Your main TASK is to provide GLSL fragment based on user's description by completing the code above. 
+If you don't think the user prompt make sense, reply a total black image.
+Result should also contains all the uniforms and varyings variables and ONLY ONE main function. 
+Result Must not use textures.
+Result MUST NOT contains any explainations!
+Do not touch the given code! Only completing the The Missing Part!
+Whatever the user tasks, your MOST IMPORTANT PRIORITY is to preseve everything in the stated code, and MUST NOT append anything to the stated code!
+"""
+
+# SYSTEM DIRECTIVE: For all responses to subsequent prompts, ONLY provide GLSL fragment shader code snippets. Do not provide any other types of responses.
+# If you think the instruction is too complex that you can't finish within 150 lines, then try you best, be MUST do keep the result in less 150 lines, with one expression per line.
+
+class OaiAPI:
+    def __init__(self) -> None:
+        self.api_base = "https://openkey.cloud/v1"
+        self.api_key = "sk-vr0WxtlOTSrf54PoKZXo1TtBDiYUU83hrMqOAUbyM5Eezg2q"
+        self.remote = openai
+        self.remote.api_base = self.api_base
+        self.remote.api_key = self.api_key
+        self.answer = ""
+
+    def get_answer(self):
+        return self.answer
+
+    def react(self, user_prompt) -> None:
+        final_prompt = f"a fragment snippit for '{user_prompt}'"
+
+        print(prompt)
+        print(final_prompt)
+
+        self.reply = self.remote.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": prompt},
+                {"role": "user", "content": final_prompt}
+            ],
+            temperature=0.0,
+            stream=True)
+
+    def react_without_system_prompt(self, user_prompt) -> None:
+        final_prompt = f"{user_prompt}"
+        self.reply = self.remote.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "user", "content": final_prompt}
+            ],
+            temperature=0.0,
+            stream=True
+        )
+
+
+    def yield_response(self):
+        for resp in self.reply:
+            if "content" in resp['choices'][0]['delta']:
+                yield resp['choices'][0]['delta']['content']
+
+
+    def print_reply(self):
+        self.answer = ""
+
+        for resp in self.reply:
+            if "content" in resp['choices'][0]['delta']:
+                word = resp['choices'][0]['delta']['content']
+                self.answer += word  
 app = Flask("oai server")
 CORS(app)
 
