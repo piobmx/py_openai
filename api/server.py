8642a22a33097cb07f8710846c817c2c1de4cb91
Zhangyi Wu <ng@MacBook-Pro.local>
2023-10-05 22:09:57 +0200

    vercel api

--- api/server.py
+++ api/server.py
@@ -126,6 +126,6 @@ def validateResult(result):
             validated_result = prefix + validated_result + "\n"
     return validated_result
 
-if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=3000, debug=True)
+# if __name__ == '__main__':
+#     app.run(host="127.0.0.1", port=3000, debug=True)
 
