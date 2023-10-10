be02e5c9ffbeed3cb9233936564ea21394009d9e
Zhangyi Wu <ng@MacBook-Pro.local>
2023-10-10 03:15:20 +0200

    api

--- api/server.py
+++ api/server.py
@@ -1,4 +1,4 @@
-import os 
+import os
 
 from flask import Flask, request, jsonify, Response
 from flask_cors import CORS
@@ -9,28 +9,51 @@ uniform vec2 u_resolution;
 uniform float u_time;
 varying vec2 vUv;
 
+// Add necessary functions here
+
 void main(){
  // [The Missing Part]
 }
 Your main TASK is to provide GLSL fragment based on user's description by completing the code above. 
 If you don't think the user prompt make sense, reply a total black image.
-Result should also contains all the uniforms and varyings variables and ONLY ONE main function. 
+Result MUST use all the uniforms and varyings.
+Result MUST contain ONLY ONE main() function. 
 Result Must not use textures.
 Result MUST NOT contains any explainations!
 Do not touch the given code! Only completing the The Missing Part!
-Whatever the user tasks, your MOST IMPORTANT PRIORITY is to preseve everything in the stated code, and MUST NOT append anything to the stated code!
+Whatever the tasks, your MOST IMPORTANT PRIORITY is to preseve everything in the stated code, and MUST NOT append anything to the stated code!  
 """
 
 # SYSTEM DIRECTIVE: For all responses to subsequent prompts, ONLY provide GLSL fragment shader code snippets. Do not provide any other types of responses.
+# Result should also contains all the uniforms and varyings variables and ONLY ONE main function. 
 # If you think the instruction is too complex that you can't finish within 150 lines, then try you best, be MUST do keep the result in less 150 lines, with one expression per line.
+
+remote_api = {
+    "azure": {
+        "api_type": "azure",
+        "api_key": os.environ['OAI_KEY_AZURE'],
+        "api_base": "https://zyoaiinstance.openai.azure.com",
+        "api_version": "2023-08-01-preview",
+    },
+    "openkey": {
+        "api_base": "https://openkey.cloud/v1"
+    }
+}
+
 
 class OaiAPI:
     def __init__(self) -> None:
-        self.api_base = "https://openkey.cloud/v1"
-        self.api_key = os.environ['OAI_KEY']
-        self.remote = openai
-        self.remote.api_base = self.api_base
-        self.remote.api_key = self.api_key
+        self.remote = "azure"
+        self.api_base = remote_api[self.remote]['api_base']
+        # self.api_key = remote_api[self.remote]['api_key']
+        self.api_key = "79febabd2a7540d7848f19dc5399129e"
+        self.api_type = remote_api[self.remote]['api_type']
+        self.api_version = remote_api[self.remote]['api_version']
+        self.openai = openai
+        self.openai.api_base = self.api_base
+        self.openai.api_key = self.api_key
+        self.openai.api_type = self.api_type
+        self.openai.api_version = self.api_version
         self.answer = ""
 
     def get_answer(self):
@@ -38,12 +61,20 @@ class OaiAPI:
 
     def react(self, user_prompt) -> None:
         final_prompt = f"a fragment snippit for '{user_prompt}'"
+        deployment_id = "gpt4-deployment"
+        if self.remote == "azure":
+            self.reply = openai.ChatCompletion.create(
+                messages=[
+                    {"role": "system", "content": prompt},
+                    {"role": "user", "content": final_prompt}
+                ],
+                deployment_id=deployment_id,
+                temperature=0.0,
+                stream=True
+            )
+            return
 
-        print(prompt)
-        print(final_prompt)
-
-        self.reply = self.remote.ChatCompletion.create(
-            model="gpt-3.5-turbo",
+        self.reply = self.openai.ChatCompletion.create(
             messages=[
                 {"role": "system", "content": prompt},
                 {"role": "user", "content": final_prompt}
@@ -53,7 +84,7 @@ class OaiAPI:
 
     def react_without_system_prompt(self, user_prompt) -> None:
         final_prompt = f"{user_prompt}"
-        self.reply = self.remote.ChatCompletion.create(
+        self.reply = self.openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[
                 {"role": "user", "content": final_prompt}
@@ -62,25 +93,41 @@ class OaiAPI:
             stream=True
         )
 
-
     def yield_response(self):
+        # x = (list(self.reply))
+        # print(x)
         for resp in self.reply:
-            if "content" in resp['choices'][0]['delta']:
-                yield resp['choices'][0]['delta']['content']
-
+            if len(resp['choices']) < 1:
+                continue
+            delta = resp['choices'][0]['delta']
+            if "content" not in delta:
+                continue
+            # print(f"delta, {delta}")
+            yield resp['choices'][0]['delta']['content']
+            # if "content" in resp['choices'][0]['delta']:
+            #     yield resp['choices'][0]['delta']['content']
 
     def print_reply(self):
         self.answer = ""
 
+        print(f"self.reply: {self.reply}")
         for resp in self.reply:
-            if "content" in resp['choices'][0]['delta']:
-                word = resp['choices'][0]['delta']['content']
-                self.answer += word  
+            print(f"resp: {resp}")
+            for r in resp: 
+                print(r)
+            # if resp['choices'].len > 0:
+            #     word = resp['choices'][0]['message']
+            #     self.answer += word
+            # if "content" in resp['choices'][0]['delta']:
+            #     word = resp['choices'][0]['delta']['content']
+            #     self.answer += word
+
 
 app = Flask("oai server")
 CORS(app)
 
 oai = OaiAPI()
+
 
 @app.route("/", methods=["GET"])
 def hello_world():
@@ -89,10 +136,10 @@ def hello_world():
 
 @app.route("/v1/api", methods=["POST"])
 def handle_prompt():
-    if isinstance(request.data, bytes):
+    if oai.api_type != "azure" and isinstance(request.data, bytes):
         prompt = request.data.decode("utf-8")
-        
-    else: 
+
+    else:
         data = request.get_json()
         if not data or "promptMessage" not in data:
             return jsonify({"error": "prompt message not provided"}), 400
@@ -100,7 +147,6 @@ def handle_prompt():
     oai.react(prompt)
     # result = oai.get_answer()
     # final_result = validateResult(result.strip())
-    # print(final_result)
     return Response(oai.yield_response(), mimetype='text/event-stream')
     # return jsonify({"ai": final_result}), 200
 
@@ -129,6 +175,6 @@ def validateResult(result):
             validated_result = prefix + validated_result + "\n"
     return validated_result
 
-# if __name__ == '__main__':
-#     app.run(host="127.0.0.1", port=3000, debug=True)
 
+if __name__ == '__main__':
+    app.run(host="127.0.0.1", port=3000, debug=True)
